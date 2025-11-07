f72f2b82588b73f8daf934f64aac992b27cf0c65
skarak1 <sudipkarak@gmail.com>
2025-11-08 00:21:31 +0530

    mongo db connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 //const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +17,18 @@ const app = express();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
